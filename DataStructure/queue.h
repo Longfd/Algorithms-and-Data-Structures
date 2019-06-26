095ddbed281804628e438089a5b149125f0f6eb7
LongFeida <longfeida.lfd@gmail.com>
2019-06-26 17:01:46 +0800

    add abstract class queue
    abstract data type specification for queue data structure

---
+++ DataStructure/queue.h
@@ -0,0 +1,23 @@
+#pragma once
+
+// abstract class queue
+// abstract data type specification for queue data structure
+// all methods are pure virtual functions
+
+template<typename T>
+class queue {
+public:
+	virtual ~queue() = default;
+
+	virtual bool empty() const = 0;
+
+	virtual int size() const = 0;
+
+	virtual T& front() = 0;
+
+	virtual T& back() = 0;
+
+	virtual void pop() = 0;
+
+	virtual void push(const T& theElemetn) = 0;
+};
