d10bcb04f6788b5c6223d3c1718ea78da4b945bb
LongFeida <longfeida.lfd@gmail.com>
2019-07-23 16:46:50 +0800

    sovle DP of  shortest path whit recursion and non-recursion
    add func:
    1.shortestPath() with recursion
    2.shortestPath2() with recursion and Remove Duplicate
    3.shortestPath3() with DP table

--- DataStructure/dynamicPrograming.cpp
+++ DataStructure/dynamicPrograming.cpp
@@ -1,8 +1,8 @@
 #include "dynamicPrograming.h"
 
-
 // A utility function returns maximum of two integers
 int max(int a, int b) { return (a > b) ? a : b; }
+int min(int a, int b) { return (a < b) ? a : b; }
 
 /*
 In the following recursion tree, K() refers to knapSack().  
@@ -83,6 +83,69 @@ int knapsack3(const int W, int wt[], int val[], const int n)
 	return max;
 }
 
+// use recursion O(2^n)
+#define ROW 4
+#define COLUMN 4
+int shortestPath(int w[][COLUMN], int i, int j)
+{
+	if (i == 0 && j == 0) return w[0][0];
+
+	if (i == 0)
+		return shortestPath(w, i, j - 1) + w[i][j];
+	else if (j == 0)
+		return shortestPath(w, i - 1, j) + w[i][j];
+	else
+		return w[i][j] + min(shortestPath(w, i, j - 1), shortestPath(w, i - 1, j));
+}
+
+// use recursion with duplicate array, < O(2^n)
+int K1[ROW][COLUMN] = { 0 };
+int shortestPath2(int w[][COLUMN], int i, int j)
+{
+	if (i == 0 && j == 0) return w[0][0];
+
+	if (K1[i][j] != 0) return K1[i][j];
+
+	if (i == 0)
+		K1[i][j] = w[i][j] + shortestPath2(w, i, j - 1);
+	else if (j == 0)
+		K1[i][j] = w[i][j] + shortestPath2(w, i - 1, j);
+	else
+		K1[i][j] = w[i][j] + min(shortestPath2(w, i, j - 1), shortestPath2(w, i - 1, j));
+
+	return K1[i][j];
+}
+
+// use formula with table O(n*m)
+/*
+  j	0	1	2	3
+i_________________		if i == 0 && j == 0  k[i][j] = matrix[0][0]
+0|  1	4	9	18		if i == 0 && j != 0  k[i][j] = k[i][j-1] + matrix[i][j]
+1|	3	4	7	11		if j == 0 && i != 0  k[i][j] = k[i-1][j] + matrix[i][j]
+2|	8	6	12	18		if j != 0 && i != 0  k[i][j] = min(k[i][j-1], k[i-1][j])
+3|	14	14	16	19
+*/
+int K[ROW][COLUMN] = { 0 };
+int shortestPath3(int w[][COLUMN], int n, int m)
+{
+	for (int i = 0; i <= n; i++) 
+	{
+		for (int j = 0; j <= m; j++)
+		{
+			if (i == 0 && j == 0)
+				K[i][j] = w[i][j];
+			else if (i == 0)
+				K[i][j] = w[i][j] + K[i][j - 1];
+			else if (j == 0)
+				K[i][j] = w[i][j] + K[i - 1][j];
+			else
+				K[i][j] = w[i][j] + min(K[i][j - 1], K[i - 1][j]);
+		}
+	}
+
+	return K[n][m];
+}
+
 void dynamicProgramingTest()
 {
 	int val[] = { 60,100,120 };
@@ -93,5 +156,12 @@ void dynamicProgramingTest()
 	cout << "knapsack():" << knapsack(W, wt, val, n) << endl;
 	cout << "knapsack2():" << knapsack2(W, wt, val, 0, n) << endl;
 	cout << "knapsack3():" << knapsack3(W, wt, val, n) << endl;
-}
 
+	int w[ROW][COLUMN] = { {1,3,5,9},
+						 {2,1,3,4},
+						 {5,2,6,7},
+						 {6,8,4,3} };
+	cout << "shortestPath():" << shortestPath(w, ROW - 1, COLUMN - 1) << endl;
+	cout << "shortestPath2():" << shortestPath2(w, ROW - 1, COLUMN - 1) << endl;
+	cout << "shortestPath3():" << shortestPath3(w, ROW - 1, COLUMN - 1) << endl;
+}
--- DataStructure/dynamicPrograming.h
+++ DataStructure/dynamicPrograming.h
@@ -5,6 +5,10 @@
 #include <iostream>
 using namespace std;
 
+// Test func
+void dynamicProgramingTest();
+
+/*****************************Knapsack Problem Begin*****************************/
 // Return the maximum value that
 // can be put in a knapsack of capacity W
 // n from n to 0, f(0) = 0
@@ -16,5 +20,40 @@ int knapsack2(int W, int wt[], int val[], int idx, int n);
 // Build table K[][] in bottom up manner
 int knapsack3(const int W, int wt[], int val[], const int n);
 
-// Test func
-void dynamicProgramingTest();
+/*****************************Knapsack Problem End*******************************/
+
+/*****************************Shortest Path Begin*****************************/
+/*
+	0	1	2	3
+_________________
+0|  1	3	5	9
+1|	2	1	3	4
+2|	5	2	6	7
+3|	6	8	4	3
+
+From (0,0) to (3,3)
+K(i,j) = min(K(i-1, j), K(i, j-1))
+
+Recursion Tree:K(i,j)  
+						K(3,3)
+					/			\
+	go up		/					 \ go left
+			K(3,2)					   K(2,3)
+		  /		   \				 / 	      \
+	   K(3,1)      K(2,2)		  K(2,2)	   K(1,3)
+	 /   \		   /    \		  /    \		 /    \
+   K(3,0) K(2,1) K(2,1) K(1,2)  K(2,1) K(1,2)  K(1,2)  K(0,3)
+
+*/
+
+// use recursion O(2^n)
+int shortestPath(int w[][4], int i, int j);
+
+// use recursion with duplicate array, < O(2^n)
+int shortestPath2(int w[][4], int i, int j);
+
+// use table O(n*m)
+int shortestPath3(int w[][4], int n, int m);
+
+/*****************************Shortest Path End*******************************/
+
